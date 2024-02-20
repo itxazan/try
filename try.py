ba5cc72eea0e97e7fec80b0d5cefe139aa05ef94
itxazan <156661331+itxazan@users.noreply.github.com>
2024-02-20 11:36:01 +0500

    try.py

--- try.py
+++ try.py
@@ -755,8 +755,6 @@ def YounisXyz(uid,pwx,tl):
             "login":"Log In"}
             xyzheader_fuck = {
     'authority': 'free.facebook.com',
-    'method': 'GET',
-    'scheme': 'https',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
